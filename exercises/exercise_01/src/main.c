5c0834446d5c0f044cf501ebcccdde970fb70193
Christopher Klix <christopher.klix@googlemail.com>
2022-12-23 11:04:22 +0100

    added main.c

---
+++ exercises/exercise_01/src/main.c
@@ -0,0 +1,82 @@
+// Allowed libraries are already included
+// DO NOT MODIFY
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+// Protoypes
+void print_int_array(int *arr, size_t arr_len);
+int sum_int_array(int *arr, size_t arr_len);
+double average_int_array(int *arr, size_t arr_len);
+
+// IMPORTANT REQUIREMENTS/LIMITATIONS
+//
+// Read the requirements and limitations carefully.
+// You can find them in the README.md file.
+
+int main(int argc, char const *argv[])
+{
+    // Predefined length of int array
+    size_t numbers_len = 50;
+
+    // Predefined pointer to array
+    int *numbers_arr;
+
+    // TODO: Write your code here
+
+    // Step 0: Fill Array with Random Numbers
+    //         Range: [0, 99] inclusive
+
+    /* Code */
+
+    // Step 1: Compute sum of the array
+
+    /* Code */
+
+    // Step 2: Compute average of the array
+
+    /* Code */
+
+    // Step 3: Print array with print_int_array()
+
+    /* Code */
+
+    // Step 4: Print sum of the array
+
+    /* Code */
+
+    // Step 5: Print average of the array
+
+    /* Code */
+
+    return 0;
+}
+
+// Implement this function
+void print_int_array(int *arr, size_t arr_len)
+{
+    /* code */
+
+    return;
+}
+
+// Implement this function
+int sum_int_array(int *arr, size_t arr_len)
+{
+    int sum = 0;
+
+    /* Code */
+
+    return sum;
+}
+
+// Implement this function
+double average_int_array(int *arr, size_t arr_len)
+{
+    double average = 0;
+
+    /* Code */
+
+    return average;
+}
