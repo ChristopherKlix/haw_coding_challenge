36f3b9a7e156a9280745e422d81efc33f2bb57e3
Christopher Klix <christopher.klix@googlemail.com>
2022-12-27 17:03:45 +0100

    added template code

---
+++ exercises/exercise_03/src/main.c
@@ -0,0 +1,40 @@
+// Allowed libraries are already included
+// You can add your own libraries if you need to
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+// ...
+
+// Macros
+// You may change these to your likings
+#define CELSIUS_OFFSET 273.15
+#define FAHRENHEIT_FRACTION 9/5
+#define FAHRENHEIT_OFFSET 32
+// ...
+
+// Protoypes
+// ...
+
+
+// IMPORTANT REQUIREMENTS/LIMITATIONS
+//
+// Read the requirements and limitations carefully.
+// You can find them in the README.md file.
+//
+// Example CLI usage:
+// $ ./temp-conv 293.15 kelvin -t celsius
+//
+// Use dedicated functions to convert temperatures.
+// Use dedicated functions to print the output.
+// stdin/stdout should not happend in the main!!!
+
+int main(int argc, char const *argv[])
+{
+    // TODO: Write your code here
+
+    // Step 0: Write the CLI 😂
+
+    /* Code */
+
+    return 0;
+}
